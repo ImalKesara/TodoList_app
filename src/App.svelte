e675e8c4cb7b009de307cd5611ded6556fcbda6e
LoneWolf <imalkesara2001@gmail.com>
2024-05-08 18:46:04 +0530

    *First*

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import '$root/styles/global.css'
+    import Todos from '$root/Components/Todo.svelte'
+    
+</script>
+
+
+
+<Todos />
---
+++ src/Components/Addtodos.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+
+    type AddTodoType = (todo:string) => void
+    type ToggleCompleted = (event:MouseEvent) =>void
+    type todoAmount = (number)
+
+
+    export let addTodo : AddTodoType
+    export let toggleCompleted :ToggleCompleted
+    export let todosamount :todoAmount
+
+    let todo = ''
+    function handleSubmit(){
+        addTodo(todo)
+        todo = ''
+    }
+</script>
+
+<form  on:submit|preventDefault={handleSubmit}>
+    {#if todosamount > 0}
+    <input type="checkbox" class="toggle-all" id ="toggle-all" on:click={toggleCompleted}>
+    <label for="toggle-all" aria-label="Marks all as complete">Marks all as complete</label>
+    <input bind:value={todo} id="new-todo" class="new-todo" placeholder="What needs to be done" type="text" autofocus>
+    {/if}
+</form>
+
+<style>
+
+    /* Add todo */
+  
+    .toggle-all {
+        width: 1px;
+        height: 1px;
+        position: absolute;
+        opacity: 0;
+    }
+    
+    .toggle-all + label {
+        position: absolute;
+        font-size: 0;
+    }
+    
+    .toggle-all + label:before {
+        content: '❯';
+        display: block;
+        padding: var(--spacing-16);
+        font-size: var(--font-24);
+        color: var(--color-gray-58);
+        transform: rotate(90deg);
+    }
+    
+    .toggle-all:checked + label:before {
+        color: var(--color-gray-28);
+    }
+    
+    .new-todo {
+        width: 100%;
+        padding: var(--spacing-16);
+        padding-left: 60px;
+        font-size: var(--font-24);
+        border: none;
+        border-bottom: 1px solid var(--shadow-1);
+    }
+    
+    /* Todo */
+    
+</style>
