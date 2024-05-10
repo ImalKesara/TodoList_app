823612bc2def77569825d14e2ed6b555248acacd
LoneWolf <imalkesara2001@gmail.com>
2024-05-10 22:35:50 +0530

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import path from "path";
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/TodoList_app/',
   plugins: [svelte()],
   resolve: {
     alias: {
-      $root: path.resolve("./src"),
+      $root: path.resolve('./src'),
     },
   },
 });
